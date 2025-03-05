0514d10eaed6c869ce222da76426e91e98fee185
saurabh <saurabh_sameer@persistent.com>
2025-03-05 23:30:24 +0530

    initial commit

---
+++ src/test/java/com/kitchome/auth/AuthebticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitchome.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthebticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
