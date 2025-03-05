01aae40563fc228398f2617653e6cc09d2c739bb
saurabh <saurabh_sameer@persistent.com>
2025-03-06 00:39:05 +0530

    added comments

--- src/main/java/com/kitchome/auth/authentication/SecurityConfig.java
+++ src/main/java/com/kitchome/auth/authentication/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.kitchome.auth.authentication;
-
-
+/*
+ * Author: saurabh sameer
+ * Date: 3-03-2025
+ * */
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,32 +10,53 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
-
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-
+// in the given code we are using form login which is 
+// stateful by default it will store a jsession id
+// check that in the browser cookies
+/*
+ * here we are configuring spring security to
+ * secure just the "/private" rest api
+ * which is not a good practice at all. we will
+ * correct all of this as we move further
+ * */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityHttpConfig(HttpSecurity http) throws Exception {
-		return http.
-				authorizeHttpRequests(authz -> authz.
-						requestMatchers("/api/v1/private").authenticated()
-						.anyRequest().permitAll())
+		return http
+				.authorizeHttpRequests(
+						authz -> authz.requestMatchers("/api/v1/private").authenticated().anyRequest().permitAll())
 				.formLogin(Customizer.withDefaults())
-				
+
 				.build();
-				
-				
-				}
+
+	}
+
 	@Bean
 	public UserDetailsService userDetailsService() {
+
 		return new InMemoryUserDetailsManager(User.builder().username("sam")
-				.password("{noop}common").authorities("ROLE_user")
-				.build()
-				);	}
-	
+				// {noop} is telling security config not to delegate
+				// any password encode and store it in plain text
+				.password("{noop}common").authorities("ROLE_user").build());
+
+	}
+
+	/*
+	 * it is reccomended to use password encoder factory it provides backward
+	 * compatiblity
+	 */
+//	@Bean
+//	public PasswordEncoder pass() {
+//		return new BCryptPasswordEncoder();
+//	}
+
 }
