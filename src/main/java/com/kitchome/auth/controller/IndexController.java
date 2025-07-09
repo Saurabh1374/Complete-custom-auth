0a10180844e5f06f7d7fb1267577bd6d2affd461
saurabh sameer <saurabhsameer21@gmail.com>
2025-07-09 13:09:00 +0530

    form login with concurrent session control

--- src/main/java/com/kitchome/auth/controller/IndexController.java
+++ src/main/java/com/kitchome/auth/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.kitchome.auth.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +10,14 @@ public class IndexController {
 	@GetMapping("/")
 	public String index(Model model) {
 		
-		model.addAttribute("loginLink", "api/v1/users/dashboard");
+		model.addAttribute("loginLink", "api/v1/users/login");
 		model.addAttribute("registerLink", "api/v1/users/register");
 
 		return "Index"; // Name of the Thymeleaf template (welcome.html)
+	}
+	@GetMapping("/invalidSession")
+	public ResponseEntity invalidSession(){
+        return ResponseEntity.ok("Under construction");
 	}
 
 }
