b003aa7cb893cfafe871be3b8f425807b48821bc
saurabh sameer <saurabhsameer21@gmail.com>
2025-08-10 16:15:48 +0530

    Jwt token and token rotation implementation

---
+++ src/main/java/com/kitchome/auth/payload/RefreshTokenRequestDTO.java
@@ -0,0 +1,14 @@
+package com.kitchome.auth.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public class RefreshTokenRequestDTO {
+        private String token;
+    }
+
--- src/main/java/com/kitchome/auth/payload/projection/RolesProjection.java
+++ src/main/java/com/kitchome/auth/payload/projection/RolesProjection.java
@@ -1,10 +1,13 @@
 package com.kitchome.auth.payload.projection;
 
 import com.kitchome.auth.util.Role;
+import org.springframework.stereotype.Component;
+
 /*
 *mapping relations in the table
 * role------1:n------>users
 */
+@Component
 public interface RolesProjection {
     Role getRole();
 }
