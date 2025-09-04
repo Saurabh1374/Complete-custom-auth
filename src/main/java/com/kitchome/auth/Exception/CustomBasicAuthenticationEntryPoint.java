f0e177cd43961f3a3e006b4df038af78ee81e32c
saurabh sameer <saurabhsameer21@gmail.com>
2025-09-04 20:04:49 +0530

    refresh token rotation error message and entrypoint and filters fixed

--- src/main/java/com/kitchome/auth/Exception/CustomBasicAuthenticationEntryPoint.java
+++ src/main/java/com/kitchome/auth/Exception/CustomBasicAuthenticationEntryPoint.java
@@ -6,10 +6,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
-
+@Component
 public class CustomBasicAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
@@ -17,7 +18,7 @@ public class CustomBasicAuthenticationEntryPoint implements AuthenticationEntryP
         String message = (authException != null && authException.getMessage() != null) ? authException.getMessage()
                 : "Unauthorized";
         String path = request.getRequestURI();
-        response.setHeader("eazybank-error-reason", "Authentication failed");
+        response.setHeader("KIC-error-reason", "Authentication failed");
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
         response.setContentType("application/json;charset=UTF-8");
         // Construct the JSON response
--- src/main/java/com/kitchome/auth/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/kitchome/auth/Exception/GlobalExceptionHandler.java
@@ -1,16 +1,26 @@
 package com.kitchome.auth.Exception;
 
+import java.io.IOException;
 import java.util.Locale;
 import java.util.Map;
 
+import com.kitchome.auth.payload.response.ErrorResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.header.Header;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kitchome.auth.payload.ValidationError;
 
+import javax.naming.AuthenticationException;
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	
@@ -36,6 +46,21 @@ public class GlobalExceptionHandler {
 		 ModelAndView mav = new ModelAndView("register");
 		    mav.addObject("errorMessage", ex.getMessage());
 		    return mav;
+	}
+	@ExceptionHandler(AuthenticationException.class)
+	public ResponseEntity<AuthenticationException> authenticationExceptionHandler(HttpServletResponse response, AuthenticationException ex) throws IOException {
+		//response.getWriter().write(ex.getMessage());
+		return new ResponseEntity<>(new HttpHeaders(),HttpStatus.UNAUTHORIZED);
+	}
+	@ExceptionHandler(AuthException.class)
+	public ResponseEntity<?> authExceptionHandler(AuthException ae){
+		log.info("caleed");
+		return new ResponseEntity<>(ErrorResponse.of(ae.getErrCode(),ae.getMessage()),ae.getErrCode().getHttpStatus());
+	}
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<?> exceptionHandler(Exception ex){
+		log.info("called");
+		return new ResponseEntity<>(ex.getMessage(),HttpStatus.BAD_REQUEST);
 	}
 
 }
